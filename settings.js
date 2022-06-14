ef8fa5b676212a2cb2eade7d809285d09d4f54a5
Pasaraba <107447224+Pasaraba@users.noreply.github.com>
2022-06-14 07:38:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94753781467']
+global.premium = ['94753781467']
+global.ownernomer = 'Pabasara JD'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
